ed55b91344bf11a4f04cd1d7a9481778cf46b5b1
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-10 15:06:12 +0530

    Logo and aims of trust added

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,13 @@
 <template>
   <section id="header" class="relative bg-white text-center">
-    <div class="pt-24 sm:pt-40 md:pt-48 lg:pt-56 px-0 pb-16 sm:pb-28 md:pb-20 lg:pb-28 border-b-2 border-[#e5e5e5] shadow-[inset_0px_-8px_0px_0px_#fff,inset_0px_-10px_0px_0px_#e5e5e5] mx-auto my-0 max-w-full w-[calc(100%-40px)] sm:w-[calc(100%-100px)] md:w-[calc(100%-80px)] lg:w-[68em] xl:w-[70em]">
+    <div class="absolute flex sm:inline w-full px-16 pt-6 sm:p-0 sm:ml-6 sm:mt-4 sm:w-32 left-0">
+      <img class="" src="~/assets/images/logo.png" alt="Tony's Trust Logo" />
+    </div>
+    <div class="pt-56 sm:pt-40 md:pt-48 lg:pt-56 px-0 pb-16 sm:pb-28 md:pb-20 lg:pb-28 border-b-2 border-[#e5e5e5] shadow-[inset_0px_-8px_0px_0px_#fff,inset_0px_-10px_0px_0px_#e5e5e5] mx-auto my-0 max-w-full w-[calc(100%-40px)] sm:w-[calc(100%-100px)] md:w-[calc(100%-80px)] lg:w-[68em] xl:w-[70em]">
 
       <!-- Logo -->
-        <h1 id="logo" class="[text-shadow:0.05em_0.075em_0px_rgba(0,0,0,0.1)] leading-9 md:leading-5 tracking-[8px] md:tracking-[13px] uppercase font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-red-400"><a class="border-0 no-underline" href="/">Strongly Typed</a></h1>
-        <p class="mt-5 sm:mt-10 tracking-widest sm:tracking-[3px] text-[#888] uppercase font-normal text-sm text-center">A responsive HTML5 site template. Manufactured by HTML5 UP.</p>
+        <h1 id="logo" class="[text-shadow:0.05em_0.075em_0px_rgba(0,0,0,0.1)] leading-9 md:leading-5 tracking-[8px] md:tracking-[13px] uppercase font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-red-400"><a class="border-0 no-underline" href="/">Tony's Education & Welfare Trust</a></h1>
+        <p class="mt-5 sm:mt-10 tracking-widest sm:tracking-[3px] text-[#888] uppercase font-normal text-sm text-center">{{ quote }}</p>
 
       <!-- Nav -->
         <nav class="hidden md:block absolute top-12 left-0 w-full cursor-default" id="nav">
@@ -16,57 +19,34 @@
 </template>
 
 <script setup lang="ts">
+var quotes = [
+  '“Education is the key to unlock the golden door of freedom” - George Washington Carver',
+  '“Education is one thing no one can take away from you.” — Elin Nordegren',
+  '“Give a man a fish and you feed him for a day; teach a man to fish and you feed him for a lifetime.” — Maimonides',
+  '“Education is what remains after one has forgotten what one has learned in school.” — Albert Einstein',
+  '“Education is not preparation for life; education is life itself.” — John Dewey',
+  '“Education is the most powerful weapon which you can use to change the world.” — Nelson Mandela',
+  '“A child without education is like a bird without wings.” — Tibetan Proverb',
+  '“Knowledge is power. Information is liberating. Education is the premise of progress in every society, in every family.” — Kofi Annan',
+  '“Education is the passport to the future, for tomorrow belongs to those who prepare for it today.” — Malcolm X',
+  '“Whatever the cost of our libraries, the price is cheap compared to that of an ignorant nation.” ― Walter Cronkite'
+];
+const quote = quotes[Math.floor(Math.random() * quotes.length)];
 var navLinks = [
   {
     href: '/',
     icon: 'fa-solid:home',
-    text: 'Introduction'
+    text: 'Home'
   },
   {
-    href: '#',
-    icon: 'fa-solid:chart-bar',
-    text: 'Dropdown',
-    children: [
-      {
-        href: '#',
-        text: 'Lorem ipsum dolor'
-      },
-      {
-        href: '#',
-        text: 'Magna phasellus'
-      },
-      {
-        href: '#',
-        text: 'Etiam dolore nisl'
-      },
-      {
-        href: '#',
-        text: 'Phasellus consequat',
-        children: [
-          {
-            href: '#',
-            text: 'Lorem ipsum dolor'
-          },
-          {
-            href: '#',
-            text: 'Magna phasellus'
-          },
-          {
-            href: '#',
-            text: 'Etiam dolore nisl'
-          }
-        ]
-      },
-      {
-        href: '#',
-        text: 'Veroeros feugiat'
-      }
-    ]
+    href: '/about',
+    icon: 'fa-solid:info',
+    text: 'About'
   },
   {
-    href: '/left-sidebar',
-    icon: 'fa-solid:cog',
-    text: 'Left Sidebar'
+    href: '/activities',
+    icon: 'fa-solid:theater-masks',
+    text: 'Activities'
   },
   {
     href: '/right-sidebar',
---
+++ pages/about.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="relative overflow-hidden bg-white py-8 sm:py-16 lg:pb-24">
+    <div class="mx-auto my-0 max-w-full w-[calc(100%-40px)] sm:w-[calc(100%-100px)] md:w-[calc(100%-80px)] lg:w-[68em] xl:w-[70em]">
+      <div id="content">
+
+        <!-- Post -->
+          <article class="border-t-2 border-[#e5e5e5] shadow-[inset_0px_8px_0px_0px_#fff,inset_0px_10px_0px_0px_#e5e5e5] mt-12 pt-12 sm:mt-20 sm:pt-20 first:border-t-0 first:shadow-none first:m-0 first:p-0 last:mb-0">
+            <header class="text-center">
+              <h2 class="text-[#888] font-normal uppercase text-lg sm:text-2xl md:text-3xl mx-0 mt-0 mb-12 tracking-widest">Who we are</h2>
+            </header>
+            <span class="transition-opacity duration-200 ease-in-out border-8 border-[#ebebeb] hover:opacity-90 block w-full mb-6 sm:mb-14"><img class="block w-full" src="~/assets/images/empowerment.jpg" alt="" /></span>
+            <h3 class="text-lg sm:text-base text-center sm:text-left tracking-widest mb-4 sm:mb-6 zinc-500 font-semibold uppercase">Introduction:</h3>
+            <p class="text-xl text-justify mb-8">
+              Tony's Education and Welfare Trust® focuses on providing education to the society and helps in the upliftment of the community through education sectors.
+            </p>
+            <h3 class="text-lg sm:text-lg text-center sm:text-left tracking-widest mb-4 sm:mb-6 zinc-500 font-semibold uppercase">Our Aims and Objectives:</h3>
+            <p class="text-justify mb-8">
+              <ol class="list-[lower-alpha] list-inside px-4 space-y-1">
+                <li class="text-lg marker:font-semibold">To take in lease, rent or to purchase, maintain, from any person, to run, maintain schools, educational institutes, and other skill development related training organizations.</li>
+                <li class="text-lg marker:font-semibold">To promote equal education of children and make such quality education available to students from all spheres irrespective of their economic, social, and cultural background.</li>
+                <li class="text-lg marker:font-semibold">To rehabilitate semi orphans, physically challenged children, neglected women, elderly people, and disrupted families in the society.</li>
+                <li class="text-lg marker:font-semibold">To grant financial assistance to any educational institution for granting scholarships, prizes, medals, awards for excellence in studies, sports and scientific research, distribution of books and notebooks for poor and or deserving students.</li>
+                <li class="text-lg marker:font-semibold">To organize and run schools under the affiliation and recognition of home schooling, IGCSE/State board/Matriculation, agencies within India.</li>
+                <li class="text-lg marker:font-semibold">To promote a greener and cleaner environment</li>
+                <li class="text-lg marker:font-semibold">To establish, maintain and subsidize schools, colleges, and libraries, educational and technical institutions.</li>
+                <li class="text-lg marker:font-semibold">To raise basic health standards of the rural masses particularly of scheduled caste / scheduled tribes / weaker section by conducting annual free medical and eye camps, and by providing medicines free of cost.</li>
+                <li class="text-lg marker:font-semibold">To work for alleviation of poverty and for getting social justice for poorer, weaker, and downtrodden section of the society, both in rural areas and urban slums focusing particularly on street children.</li>
+                <li class="text-lg marker:font-semibold">To work for the upliftment and empowerment of women by promoting literacy among girls/women, and by supporting income generation activities and other life skills.</li>
+                <li class="text-lg marker:font-semibold">To introduce simple and sustainable technologies and practices for improved water and sanitation facilities, and waste disposal.</li>
+                <li class="text-lg marker:font-semibold">To conserve, protect and promote natural resources such as water, forest, minerals etc. by popularizing afforestation/tree planting in the community, introducing water harvesting techniques, and advocating sustainable exploitation and use of minerals.</li>
+                <li class="text-lg marker:font-semibold">To work for national integration and to promote communal harmony between different religions and population groups by promoting values of tolerance, peace, and compassion in the community, and by supporting inter-faith dialogue and peace building measures where appropriate.</li>
+                <li class="text-lg marker:font-semibold">To promote and support mental health and psychological-social counseling services with an effective outreach to the vulnerable groups, especially those women who are distressed, depressed and suicidal and in need of active intervention.</li>
+                <li class="text-lg marker:font-semibold">To hold discussions, conferences and symposiums and seminars on subjects and matters associated with the objects of the trust.</li>
+                <li class="text-lg marker:font-semibold">To work in cooperation with other persons, associations, societies, institutions, foundations, and organizations, national and international, incorporated or not and to provide for representation of the trust in national and international conferences in the pursuit of all or any of the above objects of the trust.</li>
+              </ol>
+            </p>
+          </article>
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'no-sidebar'
+});
+</script>
